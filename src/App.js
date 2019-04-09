aaee0e0a9bf5b0638792e97640e995932d42c1a5
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-08 22:33:56 -0300

    Trocar o App de um component statefull para um stateless

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <h1>Vai 15 dias de React</h1>
-    );
-  }
-}
+const App = () => <h1>Vai 15 dias de React!!!</h1>
 
 export default App;
+
