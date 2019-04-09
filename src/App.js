3e6149bdb3a3a5e93db2c9d8be4cab10d194666b
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-08 22:29:57 -0300

    Explicado o básico de react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <h1>Vai 15 dias de React!!!</h1>
+      <h1>Vai 15 dias de React</h1>
     );
   }
 }
