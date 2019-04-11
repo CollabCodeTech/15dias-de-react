f5d85ce752eefdea79b7cd66c6e3018fa0ea9226
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-10 22:27:57 -0300

    Feito o desafio básico e intermediario do Dia 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 import BoardCollab from "./components/BoardCollab";
+import NextPlayer from "./components/NextPlayer";
 
-const App = () => <BoardCollab qtd="9" />;
+const App = () => (
+    <>
+        <NextPlayer />
+        <BoardCollab qtd="9" />
+    </>
+);
 
 export default App;
 
---
+++ src/components/NextPlayer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.css";
+
+const NextPlayer = () => (
+    <p className="next-player">Próximo jogador: X</p>
+);
+
+export default NextPlayer;
