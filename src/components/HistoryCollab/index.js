0dd2d3b865cd2e17a8c17d387d9e5e38d6f976ac
Marco Bruno <marco.bruno.br@gmail.com>
2019-04-17 22:49:17 -0300

    Intermediário: Colocar um pouco de CSS no component histórico

--- src/components/HistoryCollab/index.js
+++ src/components/HistoryCollab/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 
+import "./styles.css";
+
 const HistoryCollab = () => (
     <ol>
-        <li>X na 1x1</li>
-        <li>X na 1x1</li>
-        <li>X na 1x1</li>
-        <li>X na 1x1</li>
-        <li>X na 1x1</li>
+        <li className="history-collab">X na 1x1</li>
+        <li className="history-collab">X na 1x1</li>
+        <li className="history-collab">X na 1x1</li>
+        <li className="history-collab">X na 1x1</li>
+        <li className="history-collab">X na 1x1</li>
     </ol>
 );
 
